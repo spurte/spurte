5fd00f9d4d8e2fd71499e0d8c33cd09b33d05159
nikechukwu <nikechukwu@googlemail.com>
2024-08-08 16:26:10 +0000

    minor update

--- packages/dyte/lib/src/plugins/resolver.dart
+++ packages/dyte/lib/src/plugins/resolver.dart
@@ -2,6 +2,11 @@ import 'dart:io';
 
 import '../api/plugin.dart';
 
-resolve(DytePlugin plugin, String dir) {
+resolve(DytePlugin plugin, String dir) async {
   final directory = Directory(dir);
+  await for (final fse in directory.list(recursive: true)) {
+    if (fse is! File) {
+
+    }
+  }
 }
