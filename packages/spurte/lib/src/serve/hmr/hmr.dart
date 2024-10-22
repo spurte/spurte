7f83ac8cf0fcb9a11e91dd09c29ef1f0af4fe5f9
nikechukwu <nikechukwu@googlemail.com>
2024-10-22 13:55:53 +0000

    working on hmr

--- packages/spurte/lib/src/serve/hmr/hmr.dart
+++ packages/spurte/lib/src/serve/hmr/hmr.dart
@@ -1,3 +1,18 @@
+
 /// In this module we initiate HMR
 /// 
+/// 
+import 'package:embed_annotation/embed_annotation.dart';
 
+part 'hmr.g.dart';
+
+@EmbedStr('./hmr_client.js')
+const hmr_client_script = _$hmr_client_script;
+
+String addScriptToHtml(String html, String script, {bool module = false}) {
+
+}
+
+String addScriptsToHtml(String html, List<String> scripts, {bool module = false}) {
+
+}
---
+++ packages/spurte/lib/src/serve/hmr/hmr.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'hmr.dart';
+
+// **************************************************************************
+// StrEmbeddingGenerator
+// **************************************************************************
+
+const _$hmr_client_script = r'''
+
+''';
