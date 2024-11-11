cdeaa1f7b5ceabee2295b086189d6c99b0c7d3b6
--add <nikechukwu@gmail.com>
2024-11-11 09:31:30 -0600

    updated examples

--- examples/basic/web/main.dart
+++ examples/basic/web/main.dart
@@ -1,8 +1,6 @@
 import 'package:web/web.dart';
 
 void main(List<String> args) {
-  print("Hello Dart!");
-
   final element = document.createElement("div");
   element.innerHTML = "Hello Dart from Spurte!";
   document.body?.append(element);
