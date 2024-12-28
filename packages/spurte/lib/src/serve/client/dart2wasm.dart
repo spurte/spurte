796c69110023ca4722e62fc118021a12ef8d8e4b
nikechukwu <nikechukwu@googlemail.com>
2024-12-28 03:51:25 +0000

    experimental wasm

---
+++ packages/spurte/lib/src/serve/client/dart2wasm.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+import 'package:spurte/src/serve/client/common.dart';
+
+class DartWasmClientResult extends DartClientResult {
+
+}
+
+Future<DartWasmClientResult> dart2JsServer(String entrypoint, Directory dir) {
+  // TODO: Implement similar to dartDevCServer
+  throw UnimplementedError("Method not implemented");
+}
